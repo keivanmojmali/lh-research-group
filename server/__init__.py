31b275b3a1887a0c169167a300315ed4420b89fc
Keivan <keivan@Keivans-Laptop.local>
2024-04-29 11:01:28 -0400

    w

--- server/__init__.py
+++ server/__init__.py
@@ -1,22 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask
 
 
 def create_app():
-    app = Flask(
-        __name__,
-        static_folder="../client",  # Set the folder for static files
-        static_url_path="",  # Serve static files at the root URL
-        template_folder="templates",  # Location of templates relative to this file
-    )
+    app = Flask(__name__)
 
     # Register the API routes
     from .api import api as api_blueprint
 
     app.register_blueprint(api_blueprint, url_prefix="/api")
 
-    # Define a route for the homepage
-    @app.route("/")
-    def home():
-        return render_template("index.html")
+    @app.route("/health")
+    def health():
+        return {"status": "up"}
 
     return app
